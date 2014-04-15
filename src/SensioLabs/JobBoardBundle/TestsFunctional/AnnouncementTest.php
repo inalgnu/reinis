46c8f42da5909b13e11d601373c0e519dba99b0d
Reinis Grinbergs <rpg600@gmail.com>
2014-04-15 15:02:54 +0200

    Improved tests

--- src/SensioLabs/JobBoardBundle/TestsFunctional/AnnouncementTest.php
+++ src/SensioLabs/JobBoardBundle/TestsFunctional/AnnouncementTest.php
@@ -3,38 +3,36 @@
 namespace SensioLabs\JobBoardBundle\TestFunctional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\Tools\SchemaTool;
 
 class AnnouncementTest extends WebTestCase
 {
-    protected $container;
+    protected $client;
 
     public function setUp()
     {
         $kernel = static::createKernel();
         $kernel->boot();
-        $this->container = $kernel->getContainer();
+
+        $this->client = static::createClient();
     }
 
     public function testResponseIsSuccessful()
     {
-        $client = static::createClient();
+        $this->client->request('GET', '/post');
 
-        $crawler = $client->request('GET', '/post');
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->client->request('POST', '/post');
 
-        $crawler = $client->request('POST', '/post');
-
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
     public function testFormSubmissionAndRedirection()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/post');
+        $crawler = $this->client->request('GET', '/post');
 
         $form = $crawler->selectButton('Preview')->form(array(
             'announcement[title]' => 'Developer',
@@ -46,25 +44,30 @@ class AnnouncementTest extends WebTestCase
             'announcement[how_to_apply]' => 'jobs@sensiolabs.com',
         ));
 
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect('/FR/full-time/developer/preview'));
+        $this->assertEquals(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+        $this->assertTrue($this->client->getResponse()->isRedirect('/FR/full-time/developer/preview'));
 
-        $client->followRedirect();
+        $crawler = $this->client->followRedirect();
 
-        $this->assertRegExp('/Developer/', $client->getResponse()->getContent());
-        $this->assertRegExp('/SensioLabs/', $client->getResponse()->getContent());
-        $this->assertRegExp('/FR/', $client->getResponse()->getContent());
-        $this->assertRegExp('/Paris/', $client->getResponse()->getContent());
-        $this->assertRegExp('/Full Time/', $client->getResponse()->getContent());
-        $this->assertRegExp('/Some description../', $client->getResponse()->getContent());
-        $this->assertRegExp('/jobs@sensiolabs.com/', $client->getResponse()->getContent());
+        $this->assertRegExp('/Developer/', $crawler->filter('.title')->text());
+        $this->assertRegExp('/SensioLabs/', $crawler->filter('.company')->text());
+        $this->assertRegExp('/Paris, France/', $crawler->filter('.details')->text());
+        $this->assertRegExp('/Full Time/', $crawler->filter('.details')->text());
+        $this->assertRegExp('/Some description../', $crawler->filter('.description')->text());
+        $this->assertRegExp('/jobs@sensiolabs.com/', $crawler->filter('.how-to-apply')->text());
+
+        $link = $crawler->selectLink('Make changes')->link();
+
+        $this->client->click($link);
+
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
     protected function tearDown()
     {
-        $purger = new ORMPurger($this->container->get('doctrine')->getManager());
+        $purger = new ORMPurger($this->client->getContainer()->get('doctrine')->getManager());
         $purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
         $purger->purge();
 
